75f7b04d491e425dbecfb2ab41f76fbbe17aa506
bjanker <58253086+bjanker@users.noreply.github.com>
2020-07-21 16:26:03 +0200

    Update custom_databases.py
    Read taxonomy from NSR export, filter by column names instead of ID, argparse

--- script/custom_databases.py
+++ script/custom_databases.py
@@ -14,35 +14,65 @@
 # ====================================================================
 
 # Import packages
+import argparse
 import os
 import csv
 import pandas as pd
 import urllib3
 http = urllib3.PoolManager()
+csv.field_size_limit(100000000)
+
+
+# User arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-infile1', help="Input file: NSR taxonomy export")
+parser.add_argument('-output_dir', help="Output directory")
+parser.add_argument('-outfile1', help="Output file: Matching records")
+parser.add_argument('-outfile2', help="Output file: Missmatch records")
+args = parser.parse_args()
 
 
 def dutch_species_register():
     """
-    Load csv file and extract genera.
+    Loads a CSV file into a Pandas Dataframe. Isolates
+    species and genera consecutively, and converts
+    them into their respective list.
     Arguments:
-        df: csv file read as Pandas Dataframe
-        genera: subset of Dataframe containing all genera
+        df: CSV file read as Pandas Dataframe
+        scientific: Dataframe containing all species level data
+        species: Isolated species taxonomy
+        genus_only: Dataframe containing all genus level data
+        data: Species and genus level dataframes combined
+        genera: Isolated genus taxonomy
+        dataframes: list of all dataframes
+        species_list: list of all unique species
+        genera_list: list of all unique genera
     Return:
-        List containing all unique genera
+        Lists containing all unique species and genera
     """
-    # Making data frame
-    # Header line: 2nd row; Seperator: tab; Loaded colums: 1st column
-    df = pd.read_csv("testfile.csv", header=1, sep="\t", usecols=[0])
+    # Load CSV as Pandas DataFrame
+    # Define header row and seperator
+    df = pd.read_csv(args.infile1, header=2, sep="\t")
 
-    # Remove species from binomial nomenclature
-    genera = df.apply(lambda x: x.str.split().str[0])
+    # Filter out genera only records
+    scientific = df[df['rank'] != "genus"]
 
-    # Drop duplicates
-    genera.drop_duplicates(keep=False, inplace=True)
+    # Isolate species (cut off at subsp.)
+    species = scientific['scientific_name'].apply(lambda x: ' '.join(x.split()[:2]))
 
-    # Convert Pandas DataFrame into List
-    genera_list = genera["wetenschappelijke naam"].values.tolist()
-    return genera_list
+    # Add genera only records to copy of species
+    genus_only = df[df['rank'] == "genus"]
+    data = pd.concat([genus_only['scientific_name'], species])
+
+    # Isolate all genera
+    genera = data.str.split(" ", 1).str.get(0)
+
+    # Define DataFrames, drop duplicates, and covert to List
+    dataframes = [species, genera]
+    [i.drop_duplicates(inplace=True) for i in dataframes]
+    species_list, genera_list = [i.values.tolist() for i in dataframes]
+
+    return species_list, genera_list
 
 
 def bold_extract(genera):
@@ -64,9 +94,10 @@ def bold_extract(genera):
     base_url = 'http://v4.boldsystems.org/index.php/API_Public/combined?taxon='
     source_urls = list(map(lambda x: "{}{}{}".
                            format(base_url, x, '&format=tsv'), genera))
+
     # Create BOLD subdirectory if it doesn't already exists
-    if not os.path.exists('BOLD'):
-        os.makedirs('BOLD')
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
 
     # Download sequence data from BOLD using list of url's
     print('Beginning sequence data retrieval...')
@@ -75,11 +106,11 @@ def bold_extract(genera):
         r = http.request('GET', url)
         name = genera[counter]
         counter += 1
-        with open("BOLD/"+name+".tsv", "wb") as fcont:
+        with open(args.output_dir+"/"+name+".tsv", "wb") as fcont:
             fcont.write(r.data)
 
 
-def bold_nsr():
+def bold_nsr(species):
     """
     Iterating over every downloaded file from BOLD, sequence data is compared
     against species from the NSR. Subgenera will be filtered out creating a
@@ -95,32 +126,33 @@ def bold_nsr():
         tsvreader: File (genus) read in a tab delimited matter
         line: Rows of the current file (genus)
     """
-    # Load NSR species in list as reference
-    taxonomy = open("taxonomy.txt", "r")
-    species = taxonomy.read().split('\n')
 
     # Open output files for writing
-    f1 = open("bold.tsv", "w")
-    f2 = open("mismatch.tsv", "w")
+    f1 = open(args.outfile1, "w")
+    f2 = open(args.outfile2, "w")
 
     # Loop over each genus(file) downloaded from BOLD
-    for file in os.listdir("BOLD/"):
+    for file in os.listdir(args.output_dir):
         filename = os.fsdecode(file)
         if filename.endswith(".tsv"):
-            with open("BOLD/"+os.path.join(filename), errors='ignore') as tsvfile:
-                tsvreader = csv.reader(tsvfile, delimiter="\t")
+            with open(args.output_dir+"/"+os.path.join(filename), errors='ignore') as tsvfile:
+                tsvreader = csv.DictReader(tsvfile, delimiter="\t")
                 # Filter for Dutch records only
                 for line in tsvreader:
-                    if line[54] == "Netherlands":
+                    if line['country'] == "Netherlands":
                         # Skip subgenus
-                        if "sp." in str(line[21]) or "var." in str(line[21]):
+                        if "." in str(line['species_name']):
                             pass
                         # Compare genus to known species from NSR
-                        elif line[21] in species:
-                            f1.write(str(line)+"\n")
+                        elif line['species_name'] in species:
+                            for key, value in line.items():
+                                f1.write('%s\t' % (value))
+                            f1.write("\n")
                         # Write missmatches to seperate file
                         else:
-                            f2.write(str(line)+"\n")
+                            for key, value in line.items():
+                                f2.write('%s\t' % (value))
+                            f2.write("\n")
             continue
         else:
             continue
@@ -131,8 +163,8 @@ def bold_nsr():
 
 
 def main():
-    genera = dutch_species_register()
+    species, genera = dutch_species_register()
     bold_extract(genera)
-    bold_nsr()
+    bold_nsr(species)
     print("Done")
 main()
